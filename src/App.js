650324508e3104fb656b7e807cfa3ad719f3f63d
Hafeed10 <hafeex@gamil.com>
2024-10-21 17:49:27 +0530

    cart page App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Header from './Components/Header/Navbar'; // Adjust path if necessary
 import Banner from './Components/Banner/Home';
 import CardProvider from './pages/Shop/CardContext'; // Adjust path if necessary
@@ -8,20 +8,42 @@ import About from './pages/About/About';
 import Shop from './pages/Shop/Shop';  
 import CartButton from './pages/Shop/CartButton.jsx';
 import ProductDetail from './pages/Shop/ProductDetail.jsx';
+import Footer from './Components/Footer/Footer.jsx'; // Adjust path if necessary
+import FashionTrends from '../src/Components/FashionTrends/FashionTrends.jsx'
+import OfferSection from '../src/Components/OfferSection/OfferSection.jsx'
+import FashionBlog from '../src/Components/FashionBlog/FashionBlog.jsx'
+import FashionBrands from '../src/Components/FashionBrands/FashionBrands.jsx'
+import Middle from '../src/Components/Middle/Middle.jsx'
+
+
 // Import other components...
 
 const MainContent = () => {
+    const location = useLocation(); // Fix the missing variable name
+
     return (
         <div className="App">
             <Header />
             <Routes>
+            <Route path="/" element={
+          <>
+            <Banner />
+            <FashionTrends />
+            <OfferSection />
+            <FashionBlog />
+            <FashionBrands />
+            <Middle />
+          </>
+        } />
                 <Route path="/" element={<Banner />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/shop" element={<Shop />} />
                 <Route path="/another" element={<CartButton />} />
                 <Route path="/product/:id" element={<ProductDetail />} />
-
             </Routes>
+            
+            {/* Conditionally render Footer if not on '/another' route */}
+            {location.pathname !== '/another' && <Footer />}
         </div>
     );
 };
